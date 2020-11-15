6e35daaedb46e133d63bcbd3b502e85a10b2319f
Runar Saur Modahl <runarmod@gmail.com>
2020-11-15 20:02:17 +0100

    Fixed kerning on some print statements

--- menu.py
+++ menu.py
@@ -15,7 +15,7 @@ def menu():
 
 
 def create():
-    print('Please proivide the name of the site or app you want to generate a password for')
+    print('Please proivide the name of the site or app you want to generate a password for: ')
     app_name = input()
     print('Please provide a simple password for this site: ')
     plaintext = input()
@@ -26,22 +26,21 @@ def create():
     print('Your password has now been created and copied to your clipboard')
     print('')
     print('-' * 30)
-    user_email = input('Please provide a user email for this app or site')
-    username = input(
-        'Please provide a username for this app or site (if applicable)')
+    user_email = input('Please provide a user email for this app or site: ')
+    username = input('Please provide a username for this app or site (if applicable): ')
     if username == None:
         username = ''
-    url = input('Please paste the url to the site that you are creating the password for')
+    url = input('Please paste the url to the site that you are creating the password for: ')
     store_passwords(passw, user_email, username, url, app_name)
 
 
 def find():
-    print('Please proivide the name of the site or app you want to find the password to')
+    print('Please proivide the name of the site or app you want to find the password to: ')
     app_name = input()
     find_password(app_name)
 
 
 def find_accounts():
-    print('Please proivide the email that you want to find accounts for')
+    print('Please proivide the email that you want to find accounts for: ')
     user_email = input()
     find_users(user_email)

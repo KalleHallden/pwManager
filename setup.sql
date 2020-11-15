203b8720051672796cc91cc033d00895cf1aa2a4
Anthony <amdecapio@gmail.com>
2020-11-15 18:14:17 -0500

    Fixed Error

--- setup.sql
+++ setup.sql
@@ -2,9 +2,9 @@ CREATE DATABASE IF NOT EXISTS pwmanager;
 use pwmanager;
 
 CREATE TABLE IF NOT EXISTS accounts (
-    password VARCHAR,
-    email VARCHAR,
-    username VARCHAR,
-    url VARCHAR,
-    app_name VARCHAR
+    password VARCHAR(255),
+    email VARCHAR(255),
+    username VARCHAR(255),
+    url VARCHAR(255),
+    app_name VARCHAR(255)
 );

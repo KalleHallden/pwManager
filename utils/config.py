ab32ffeae485bd00bc86edbd44aaa1cd74f7132d
AnonymeKnight <tnizar019@gmail.com>
2020-11-30 09:55:20 +0100

    nothing special

--- utils/config.py
+++ utils/config.py
@@ -1,6 +1,1 @@
-islaunched = True
-user_name = 'Anonymous'
-password = b'gAAAAABfxJiKRP06KJGgluZTdwtGu1T9LLy27G8XK562tnG4KDmRnbrfo2U4BKG7NkQ-P4aH71iHnHQN4NJPQ-Iy0nslhqszGw=='
-dbuser = 'postgres'
-dbname = b'gAAAAABfxJiUWkiZfDy9WBVsHNxybBqHlcNliDngodTPaPZn3ZYSBY3mlwu-Y7PXiQl0nvcG1OpWh5kDNJYhej0JPm0JyKmg1g=='
-dbpw = 'accounts'
+islaunched = False
